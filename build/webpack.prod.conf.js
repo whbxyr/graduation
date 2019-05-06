1cfff9c5a19750dd8b470b597bfab6c7aa50433a
whbxyr <whbxyr@gmail.com>
2019-05-06 23:29:08 +0800

    打包方式更新

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -73,7 +73,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       chunksSortMode: 'dependency'
     }),
     // keep module.id stable when vendor modules does not change
-    new webpack.NamedChunksPlugin(),
+    // new webpack.NamedChunksPlugin(),
     new webpack.HashedModuleIdsPlugin(),
     // copy custom static assets
     new CopyWebpackPlugin([
